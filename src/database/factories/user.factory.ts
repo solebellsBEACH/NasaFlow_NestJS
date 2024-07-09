5874703318a65781a9d198a141db884de730d01d
solebellsBEACH <lucassxxavier@gmail.com>
2024-07-08 23:57:09 -0300

    feat: adding user seed

---
+++ src/database/factories/user.factory.ts
@@ -0,0 +1,7 @@
+import { User } from "@users/user.entity";
+import { setSeederFactory } from "typeorm-extension";
+
+export const userFactory = setSeederFactory(User, async (faker) => {
+    const user = new User({ username: faker.name.fullName(), password: faker.internet.password(), });
+    return user
+})
